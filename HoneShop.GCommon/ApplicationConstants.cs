d0c7fac49c82560a28c6080a3e5090c81d01c41a
Tonchev77 <tonitonchev96@gmail.com>
2025-07-25 20:50:27 +0300

    Implement notification for Actions in System

--- HoneShop.GCommon/ApplicationConstants.cs
+++ HoneShop.GCommon/ApplicationConstants.cs
@@ -11,5 +11,11 @@
         public const string UserRoleName = "User";
         public const string AdminRoleName = "Admin";
         public const string AdminAreaName = "Admin";
+
+
+        public const string InfoMessageKey = "info";
+        public const string WarningMessageKey = "warning";
+        public const string ErrorMessageKey = "error";
+        public const string SuccessMessageKey = "success";
     }
 }
---
+++ HoneShop.GCommon/NotificationMessages.cs
@@ -0,0 +1,15 @@
+﻿namespace HoneyShop.GCommon
+{
+    public class NotificationMessages
+    {
+        public static class Category
+        {
+            public const string CategoryAddedSuccessfully = "Category added successfully.";
+            public const string CategoryEditedSuccessfully = "Category edited successfully.";
+            public const string CategoryDeletedSuccessfully = "Category deleted successfully.";
+            public const string CategoryNotFound = "Category not found.";
+            public const string CategoryError = "Fatal error occurred while updating the category!";
+            public const string CategoryAlreadyExists = "Category with this name already exists.";
+        }
+    }
+}
