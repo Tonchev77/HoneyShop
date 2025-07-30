8f7406389226ea15f12bbfd47b401da8a2f2b31b
Tonchev77 <tonitonchev96@gmail.com>
2025-07-31 01:10:03 +0300

    Added view for Contact

---
+++ HoneyShop/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿namespace HoneyShop.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    public class ContactController : BaseController
+    {
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
